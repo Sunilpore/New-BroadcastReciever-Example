f8821b48d51d012c317d213397d437bbac2f1dc9
Sunilpore <sunilpore20@gmail.com>
2017-11-19 10:41:41 -0800

    Used either manifest one or activity one to avoid crash app

---
+++ BroadcastReciever_1.1/app/src/main/java/com/example/sunil/broadcastreciever_11/InternetConnect_Reciever.java
@@ -0,0 +1,75 @@
+package com.example.sunil.broadcastreciever_11;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.widget.Toast;
+
+/**
+ * Created by Sunil on 11/19/2017.
+ */
+
+public class InternetConnect_Reciever extends BroadcastReceiver {
+
+    private static final String ActA="register_via_activity";
+    private static final String ActB="android.net.conn.CONNECTIVITY_CHANGE";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+            ConnectivityManager cm= (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+            NetworkInfo nwinfo=cm.getActiveNetworkInfo();
+
+            Boolean isConnected= nwinfo!=null && nwinfo.isConnectedOrConnecting();
+
+
+        if(intent.getAction().equals(ActA)) {
+            if (isConnected)
+                Toast.makeText(context, "Network is connected", Toast.LENGTH_LONG).show();
+            else
+                Toast.makeText(context, "Network is not connected", Toast.LENGTH_LONG).show();
+
+        }
+        else if(intent.getAction().equals(ActB))
+        if (isConnected)
+            Toast.makeText(context, "Network is connected via Manifest", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(context, "Network is disconnected via Manifest ", Toast.LENGTH_LONG).show();
+    }
+
+
+
+}
+
+
+
+
+    /*//This can used when not implement 'BroadcastReceiver' to class 'InternetConnect_Reciever' class
+    BroadcastReceiver A=new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            if(intent.getAction().equals(ActA)){
+
+                ConnectivityManager cm= (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+                NetworkInfo nwinfo=cm.getActiveNetworkInfo();
+
+                Boolean isConnected= nwinfo!=null && nwinfo.isConnectedOrConnecting();
+
+                if(isConnected)
+                    try {
+                        Toast.makeText(context, "Network is connected", Toast.LENGTH_LONG).show();
+                    }
+                    catch(Exception e){
+                        e.printStackTrace();
+                    }
+                else
+                        Toast.makeText(context, "Network is not connected", Toast.LENGTH_LONG).show();
+
+                }
+        }*/
+
---
+++ BroadcastReciever_1.1/app/src/main/java/com/example/sunil/broadcastreciever_11/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.sunil.broadcastreciever_11;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String ActA="register_via_activity";
+    private static final String ActB="android.net.conn.CONNECTIVITY_CHANGE";
+    Intent a;
+    Button status;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerReceiver(new InternetConnect_Reciever(),new IntentFilter(ActA));
+        registerReceiver(new InternetConnect_Reciever(),new IntentFilter(ActB));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       status= (Button) findViewById(R.id.bt_status);
+
+        a=new Intent(ActA);
+        a=new Intent(ActB);
+
+        status.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                sendBroadcast(a);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(new InternetConnect_Reciever());
+    }
+
+   /* BroadcastReceiver A= new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+
+        }
+    };*/
+
+
+}
